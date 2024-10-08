75107245441668780c51e00ccee94804baf8fb24
Ahmed Khalil <ahmed.khalil@haw-hamburg.de>
2024-10-09 00:32:26 +0200

    Refaktorisiert

--- src/main/java/de/hamburg/comicdatabase_backend/foundation/dataaccess/api/entity/Publisher.java
+++ src/main/java/de/hamburg/comicdatabase_backend/foundation/dataaccess/api/entity/Publisher.java
@@ -1,21 +1,20 @@
-package de.hamburg.comicdatabase_backend.datatype;
+package de.hamburg.comicdatabase_backend.foundation.dataaccess.api.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@RequiredArgsConstructor
 
 public class Publisher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NonNull
     @Column(nullable = false, unique = true)
     private String publisherName;
 }
